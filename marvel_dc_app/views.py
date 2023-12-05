d7a11e7eb214c1cdef21b99aff967f1a55bb8d2d
Dianisa Wulandari <dianisa.wulandari@ui.ac.id>
2023-12-05 11:30:50 +0700

    feat(film detail): create query

--- marvel_dc_app/views.py
+++ marvel_dc_app/views.py
@@ -7,8 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 namespace = "kb"
-# set host dan port pake url blazegraph local/remote
-sparql = SPARQLWrapper("http://192.168.249.48:9999/bigdata/namespace/"+ namespace + "/sparql")
+# set host pake url blazegraph local/remote
+host = "http://10.5.88.151:9999/"
+sparql = SPARQLWrapper(f"{host}bigdata/namespace/"+ namespace + "/sparql")
 sparql.setReturnFormat(JSON)
 
 
@@ -18,9 +19,8 @@ def search_result(request):
     search = request.POST['search']
     search = search.lower()
 
-  # set prefix : pake url blazegraph local/remote, ubah juga prefix di ttlnya
-    sparql.setQuery("""
-    prefix :      <http://192.168.249.48:9999/>
+    sparql.setQuery(f"""
+    prefix :      <{host}>
     prefix owl:   <http://www.w3.org/2002/07/owl#>
     prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
@@ -29,7 +29,7 @@ def search_result(request):
     prefix xsd:   <http://www.w3.org/2001/XMLSchema#>
 
     SELECT DISTINCT ?film_wiki_uri ?film_name ?year ?film_type ?runtime ?mpa_rating ?desc ?crit_cons ?director (group_concat(distinct ?star;separator=", ") as ?stars) (group_concat(distinct ?distributor;separator=", ") as ?distributors) (group_concat(distinct ?genre;separator=", ") as ?genres) ?imdb_gross ?imdb_rating ?imdb_votes ?tom_aud_score ?tom_ratings ?tomato_meter ?tomato_review 
-    WHERE{
+    WHERE{{
         ?film_wiki_uri rdf:type :Film;
                         rdfs:label ?film_name .
         FILTER contains(LCASE(?film_name),"%s")
@@ -56,7 +56,7 @@ def search_result(request):
         
         ?star_wiki_uri rdfs:label ?star.
         
-    }
+    }}
     GROUP BY ?film_wiki_uri ?film_name ?year ?film_type ?runtime ?mpa_rating ?desc ?crit_cons ?director ?imdb_gross ?imdb_rating ?imdb_votes ?tom_aud_score ?tom_ratings ?tomato_meter ?tomato_review 
 	ORDER BY ?film_name
     """ % search)
@@ -64,10 +64,9 @@ def search_result(request):
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     response['data'] = results["results"]["bindings"]
-    
-  # set prefix : pake url blazegraph local/remote, ubah juga prefix di ttlnya
-    sparql.setQuery("""
-    prefix :      <http://192.168.249.48:9999/>
+
+    sparql.setQuery(f"""
+    prefix :      <{host}>
     prefix owl:   <http://www.w3.org/2002/07/owl#>
     prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
@@ -76,10 +75,10 @@ def search_result(request):
     prefix xsd:   <http://www.w3.org/2001/XMLSchema#>
 
     SELECT DISTINCT ?film_name 
-    WHERE{
+    WHERE{{
         ?film rdf:type :Film;
                 rdfs:label ?film_name .
-    }
+    }}
     """)
     results_2 = sparql.query().convert()
     data_2 = results_2["results"]["bindings"]
@@ -116,3 +115,55 @@ def search_result_person(request):
 def search_result_film(request):
     response = {}
     return render(request, 'search_film.html', response)
+
+
+@csrf_exempt
+def get_film_detail(request):
+    response = {}
+    film_wiki_uri = request.POST['film_wiki_uri']
+    film_wiki_uri = film_wiki_uri.replace("http://www.wikidata.org/entity/", "wd:")
+
+    sparql.setQuery(f"""
+      prefix :      <{host}>
+      prefix owl:   <http://www.w3.org/2002/07/owl#>
+      prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+      prefix vcard: <http://www.w3.org/2006/vcard/ns#>
+      prefix wd:    <http://www.wikidata.org/entity/>
+      prefix xsd:   <http://www.w3.org/2001/XMLSchema#>
+
+    SELECT DISTINCT ?film_name ?year ?film_type ?runtime ?mpa_rating ?desc ?crit_cons ?director (group_concat(distinct ?star;separator=", ") as ?stars) (group_concat(distinct ?distributor;separator=", ") as ?distributors) (group_concat(distinct ?genre;separator=", ") as ?genres) ?imdb_gross ?imdb_rating ?imdb_votes ?tom_aud_score ?tom_ratings ?tomato_meter ?tomato_review
+    WHERE{{
+        {film_wiki_uri} rdf:type :Film;
+                       rdfs:label ?film_name; 
+                       :year ?year; 
+                       :entity ?film_type; 
+                       :runtime ?runtime;
+                       :mpa_rating ?mpa_rating;
+                       :description ?desc;
+                       :crit_consensus ?crit_cons;
+                       :director ?director_wiki_uri;
+                       :imdb_gross ?imdb_gross;
+                       :imdb_rating ?imdb_rating;
+                       :imdb_votes ?imdb_votes;
+                       :tom_aud_score ?tom_aud_score;
+                       :tom_ratings ?tom_ratings;
+                       :tomato_meter ?tomato_meter;
+                       :tomato_review ?tomato_review;
+                       :genre ?genre;
+                       :distributed_by ?distributor;
+                       :stars ?star_wiki_uri .
+        
+        ?director_wiki_uri rdfs:label ?director.
+        
+        ?star_wiki_uri rdfs:label ?star.
+        
+    }}
+    GROUP BY ?film_name ?year ?film_type ?runtime ?mpa_rating ?desc ?crit_cons ?director ?imdb_gross ?imdb_rating ?imdb_votes ?tom_aud_score ?tom_ratings ?tomato_meter ?tomato_review 
+    """)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    response['data'] = results["results"]["bindings"]
+    # return render(request, 'player_detail.html', response)
+    return JsonResponse(response, status=200)
