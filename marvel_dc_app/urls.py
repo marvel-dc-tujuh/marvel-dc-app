bce46c1abdbb976bb983ee45b86b7f169a661719
cinvetsin <sashanabilafortuna@gmail.com>
2023-12-08 11:18:23 +0700

    resolve conflict

--- marvel_dc_app/urls.py
+++ marvel_dc_app/urls.py
@@ -5,8 +5,6 @@ app_name = 'marvel_dc_app'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('search_result/person', search_result_person, name="search_result_person"),
-    path('search_result/film', search_result_film, name="search_result_film"),
     path('search', search_result, name='search_result'),
     path('film', get_film_detail, name='get_film_detail'),
     path('person', get_person_detail, name='get_person_detail')
--- marvel_dc_app/views.py
+++ marvel_dc_app/views.py
@@ -102,14 +102,6 @@ def search_result(request):
 def index(request):
     return render(request, 'index.html')
 
-def search_result_person(request):
-    response = {}
-    return render(request, 'search_person.html', response)
-
-def search_result_film(request):
-    response = {}
-    return render(request, 'search_film.html', response)
-
 @csrf_exempt
 def get_film_detail(request):
     response = {}
